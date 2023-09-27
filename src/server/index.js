e78333dd056b3edb08bbb3b9269efc36d525796a
Nick Topacio <topacio.3@osu.edu>
2023-09-27 12:13:09 -0400

    Add back in public folder

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,8 @@ import database from "../scripts/database.js";
 
 const app = express();
 
-// app.use(express.static("public")); // serve static files from public folder
-// app.use(express.json({ limit: "1mb" })); // parse JSON data from request body
+app.use(express.static("public")); // serve static files from public folder
+app.use(express.json({ limit: "1mb" })); // parse JSON data from request body
 
 // GET request handler for /api/articles
 app.get("/api/tech-articles", async (request, response) => {
