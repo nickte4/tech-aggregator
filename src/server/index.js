f73bef25f9fee46e948a11f1cf6c9445cfa02366
Nick Topacio <topacio.3@osu.edu>
2023-09-27 11:48:02 -0400

    Remove port listen

--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,9 @@ import express from "express";
 import database from "../scripts/database.js";
 
 const app = express();
-const port = 3000; // Port to listen on
 
-// open port 3000 for listening
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-app.use(express.static("public")); // serve static files from public folder
-app.use(express.json({ limit: "1mb" })); // parse JSON data from request body
+// app.use(express.static("public")); // serve static files from public folder
+// app.use(express.json({ limit: "1mb" })); // parse JSON data from request body
 
 // GET request handler for /api/articles
 app.get("/api/tech-articles", async (request, response) => {
