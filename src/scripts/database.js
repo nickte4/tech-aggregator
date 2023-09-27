c39e9480a7ac43dd891c57a32cb9d26c4897632f
Nick Topacio <topacio.3@osu.edu>
2023-09-26 23:51:43 -0400

    Complete simple DB storage of tech-articles, refreshes articles every 24 hrs

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,6 +1,7 @@
 /* simple database functionality */
 import aggregator from "../scripts/index.js";
-import fs from "fs";
+import fsSync from "fs";
+import fs from "fs/promises";
 const DB_FILE_NAME = "./src/assets/tech-article-db.txt";
 
 /*
@@ -16,28 +17,30 @@ async function getTechArticles() {
     tech articles to it. Otherwise, do nothing.
 */
 async function initializeDatabase() {
-  if (!fs.existsSync(DB_FILE_NAME)) {
-    updateDatabase(await getTechArticles());
+  if (!fsSync.existsSync(DB_FILE_NAME)) {
+    let articleList = await getTechArticles();
+    await updateDatabase(articleList);
   }
 }
 
 /* 
     updateDatabase: writes the tech articles to the database file
 */
-function updateDatabase(techArticles) {
+async function updateDatabase(techArticles) {
+  const data = JSON.stringify(techArticles);
   // name of database file to write to
-  fs.writeFile(DB_FILE_NAME, JSON.stringify(techArticles), (err) => {
-    if (err) {
-      console.error("Error updating database file: ", err);
+  await fs.writeFile(DB_FILE_NAME, data, (writeErr) => {
+    if (writeErr) {
+      console.error("Error updating database file: ", writeErr);
+    }
+  });
+  // append timestamp to database file
+  await fs.appendFile(DB_FILE_NAME, `\n${Date.now()}`, (appendErr) => {
+    if (appendErr) {
+      console.error("Error appending timestamp to database file: ", appendErr);
     }
   });
 
-  // add a timestamp to the database file
-  fs.appendFile(DB_FILE_NAME, `\n${Date.now()}`, (err) => {
-    if (err) {
-      console.error("Error appending timestamp to database file: ", err);
-    }
-  });
   return techArticles;
 }
 
@@ -45,19 +48,19 @@ function updateDatabase(techArticles) {
     readDatabase: reads the database file containing the tech articles
     and returns the parsed JSON data with a timestamp
 */
-function readDatabase() {
-  fs.readFile(DB_FILE_NAME, (err, data) => {
+async function readDatabase() {
+  const data = await fs.readFile(DB_FILE_NAME, (err) => {
     if (err) {
       console.error("Error reading database file: ", err);
     }
-    // split the file contents into an array of strings
-    const lines = data.toString().split("\n");
-    // exclude the last line
-    const techArticles = JSON.parse(lines.slice(0, -1).join("\n"));
-    // get last line, which is the timestamp
-    const timestamp = lines[lines.length - 1];
-    return { techArticles, timestamp };
   });
+  // split the file contents into an array of strings
+  const lines = data.toString().split("\n");
+  // exclude the last line
+  const techArticles = JSON.parse(lines.slice(0, -1).join("\n"));
+  // get last line, which is the timestamp
+  const timestamp = lines[lines.length - 1];
+  return { techArticles, timestamp };
 }
 
 /*
@@ -71,7 +74,8 @@ async function retrieveData() {
   const timeDifferenceInHours = (currentDate - timestamp) / 1000 / 60 / 60;
   if (timeDifferenceInHours >= 24) {
     // update the database and return the updated tech articles
-    return updateDatabase(await getTechArticles());
+    let articleList = await getTechArticles();
+    return await updateDatabase(articleList);
   } else {
     // return the tech articles from the database
     return techArticles;
--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use(express.static("public")); // serve static files from public folder
 app.use(express.json({ limit: "1mb" })); // parse JSON data from request body
 
 // GET request handler for /api/articles
-app.get("/api/tech-articles", (request, response) => {
+app.get("/api/tech-articles", async (request, response) => {
   console.log("GET request received at /api/tech-articles");
-  response.json(database.retrieveData());
+  response.json(await database.retrieveData());
 });
